325baaa522ed11e16ba125da3971286224549aae
YOGEESH K P <yogeesh793@gmail.com>
2024-12-25 11:43:58 +0530

    Add files via upload

---
+++ Problem_8.py
@@ -0,0 +1,11 @@
+"""Bitwise Operater: given two integer, write a python program.
+* prints the results of a & b, a | b and a ^ b.
+* Shifts the bits of a two position to the left(a<<2).
+* Shifts the bits of b one position to the right(b>>1). """
+
+a, b=5, 6
+print(a & b)
+print(a | b)
+print(a ^ b)
+print(a<<2)
+print(b>>1)
